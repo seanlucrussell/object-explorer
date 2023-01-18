414320808e07f723acf994dc6d2cc4845e4c805b
Sean Russell <seanlucrussell@gmail.com>
2023-01-17 21:03:18 -0700

    Making the script more flexible by accepting git directory as input. Also adding some more checks for better error messaging

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,14 @@
 module Main where
 
-import Control.Monad
+import Control.Exception (Exception, IOException, catch)
 import Parsers (parseObject, repoSummaryParser)
-import Renderers
+import Renderers (renderObject, renderSummary)
+import System.Environment
+import System.Exit (exitFailure)
 import System.Process (readProcess)
 import Text.Blaze.Html.Renderer.String (renderHtml)
 import Text.Parsec (ParseError, parse)
-import Types
+import Types (Object, ObjectSummary (hash))
 
 catObject :: String -> String -> IO String
 catObject path hash = readProcess "git" ["-C", path, "cat-file", "-p", hash] []
@@ -14,21 +16,17 @@ catObject path hash = readProcess "git" ["-C", path, "cat-file", "-p", hash] []
 listAllObjects :: String -> IO String
 listAllObjects path = readProcess "git" ["-C", path, "cat-file", "--batch-check", "--batch-all-objects"] []
 
---repo = "../git-exploration"
-
-repo = "."
-
 output = "./generated/"
 
-grabObject :: ObjectSummary -> IO (Either ParseError Object)
-grabObject o = do
+grabObject :: String -> ObjectSummary -> IO (Either ParseError Object)
+grabObject repo o = do
   o <- catObject repo (hash o)
   return (parse parseObject "" o)
 
-printObjectFromSummary :: ObjectSummary -> IO ()
-printObjectFromSummary s = do
+printObjectFromSummary :: String -> ObjectSummary -> IO ()
+printObjectFromSummary repo s = do
   let ref = hash s
-  o <- grabObject s
+  o <- grabObject repo s
   case o of
     Left pe -> putStrLn ("Failed to read " ++ ref)
     Right ob ->
@@ -36,15 +34,31 @@ printObjectFromSummary s = do
 
 main :: IO ()
 main = do
-  s <- listAllObjects repo
-  let repoSummary = parse repoSummaryParser "" s
-  case repoSummary of
-    Left _ -> putStrLn "Error generating list of all objects in repo"
-    Right summary -> do
-      print summary
-      writeFile (output ++ "overview.html") (renderHtml (renderSummary summary))
-      mapM_ printObjectFromSummary summary
+  args <- getArgs
+  if length args /= 1
+    then do
+      putStrLn "Error: this script requires the path to a git repo as input"
+      exitFailure
+    else do
+      let repo = head args
+      s <-
+        catch
+          (listAllObjects repo)
+          ( (const :: (IO String -> IOException -> IO String))
+              ( do
+                  putStrLn $ "Can't read git data from \"" ++ repo ++ "\""
+                  exitFailure
+              )
+          )
+      let repoSummary = parse repoSummaryParser "" s
+      case repoSummary of
+        Left _ -> putStrLn "Error generating list of all objects in repo"
+        Right summary -> do
+          print summary
+          writeFile (output ++ "overview.html") (renderHtml (renderSummary summary))
+          mapM_ (printObjectFromSummary repo) summary
 
 -- tooltips: match output of git commands as closely as possible except for header info, add tooltips to provide more detail.
--- validate repo exists/git is installed
+
+-- choose generated path
 -- don't worry about files being well formed, just print error for hash (parse failed, content of body) and continue on
